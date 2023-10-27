e280a780f263b28f8664fe703d5f51116aadeca6
Samarth-Kamble <kamble.samarth91@gmail.com>
2023-10-27 22:12:12 +0530

    Project Setup and the divide web page in the required section and modified the tailwind config and index.css make a tailwind css class

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x  py-10">Services</section>
+    <section className="padding">SpecialOffers</section>
+    <section className="bg-pale-blue padding">Customer Review</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
+
+export default App;
