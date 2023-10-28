c1c4f76586185796b940d71f4e3cfaa29f0825ed
Samarth-Kamble <kamble.samarth91@gmail.com>
2023-10-28 22:24:49 +0530

    Complete the nike landing page completed a hero section and all remaining section

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
-# React + Vite
+# Tailwind CSS Full Course 2023 | Build and Deploy a Nike Website
+![Nike Landing Page](https://i.ibb.co/pxzMGb4/Thumbnail-2.png)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+### [🌟 Become a top 1% Next.js 13 developer in only one course](https://jsmastery.pro/next13)
+### [🚀 Land your dream programming job in 6 months](https://jsmastery.pro/masterclass)
 
-Currently, two official plugins are available:
+## Tailwind CSS Starter Kit
+📚 https://resource.jsmastery.pro/tailwind-starter-kit
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Summary
+Dive into the world of Tailwind CSS, build a Nike website, and join top-tier organizations like OpenAI, Shopify, and NASA in building stunning apps effortlessly.
+
+In this course, you'll:
+- Learn how to make the most out of Tailwind CSS
+- Fully understand how Tailwind works behind-the-scenes
+- Follow all of Tailwind's best practices
+- Put all of your newly gained Tailwind skills to the test by building a beautiful Nike landing page
+  
+## Want to land your dream programming job in 3 - 6 months?
+⭐ JSM Masterclass Experience - https://jsmastery.pro/masterclass
+Become a Software Engineer. Guaranteed.
--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,45 @@
+import { Nav } from "./components";
 import {
+  CustomerReviews,
+  Footer,
   Hero,
   PopularProducts,
-  SuperQuality,
   Services,
-  SpecialOffers,
-  CustomerReview,
+  SpecialOffer,
   Subscribe,
-  Footer,
-} from "./section";
-import Nav from "./components/Nav";
-const App = () => (
-  <main className="relative">
-    {/* Nav */}
-    <Nav />
-    {/* Hero Container  */}
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero />
-    </section>
+  SuperQuality,
+} from "./sections";
 
-    {/* Popular Product Container */}
-    <section className="padding">
-      <PopularProducts />
-    </section>
-
-    {/* SuperQuality Container */}
-    <section className="padding">
-      <SuperQuality />
-    </section>
-
-    {/* Services Container */}
-    <section className="padding-x  py-10">
-      <Services />
-    </section>
-
-    {/* SpecialOffer Containe */}
-    <section className="padding">
-      <SpecialOffers />
-    </section>
-
-    {/* Customer Review */}
-    <section className="bg-pale-blue padding">
-      <CustomerReview />
-    </section>
-
-    {/* Subscribe Container */}
-    <section className="padding-x sm:py-32 py-16 w-full">
-      <Subscribe />
-    </section>
-
-    {/* Footer Container */}
-    <section className="bg-black padding-x padding-t pb-8">
-      <Footer />
-    </section>
-  </main>
-);
+const App = () => {
+  return (
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
 
 export default App;
